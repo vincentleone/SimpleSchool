4204211d712b44708bfee3836aa3331ff2f84810
gabrielrapaport <gabrielrapaport@gmail.com>
2021-12-10 13:07:41 -0500

    class list 50%

--- app/userclass/lib/components/userclass.dart
+++ app/userclass/lib/components/userclass.dart
@@ -1,24 +1,65 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
-class UserClass extends StatelessWidget {
-  static const _biggerFont = TextStyle(fontSize: 40.0,color: Color.blueGrey);
+class UserClasses extends StatefulWidget {
   @override
-  //State<UserClass> createState() => _UserClass();
+  _UserClassesState createState() => _UserClassesState();
+}
 
-  const classList = <Widget> [];
-  const listTile = (title: Text(style: _biggerFont,));
+class _UserClassesState extends State<UserClasses> {
+  // Assume DB Call here
+  final _style = TextStyle(fontSize: 40.0, color: Colors.blueGrey);
+  final List<String> classList = <String>[
+    "Calc1",
+    "Calc2",
+    "Calc3",
+    "Automata",
+    "Parallel Computing",
+    "Network Science"
+  ];
+  final colors = [Colors.blue, Colors.lightBlue];
 
+  final isSelected = <bool>[];
+
+  Widget _buildClassList() {
+    return ListView.separated(
+      padding: const EdgeInsets.all(16),
+      itemCount: classList.length,
+      itemBuilder: (BuildContext context, int index) {
+        isSelected.add(false);
+        return ElevatedButton.icon(
+          style: ElevatedButton.styleFrom(
+                primary: Colors.white,
+                padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                textStyle: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold)),
+          onPressed: () => {
+            setState(() {
+              isSelected[index] = !isSelected[index];
+            })
+          },
+          icon: Icon(isSelected[index] ? Icons.circle : Icons.circle_outlined,
+              color: Colors.lightBlue),
+
+          // background: MaterialStateProperty.all<Color>(red) ,
+          label: Text(
+            '${classList[index]}',
+            style: TextStyle(color: Colors.black),
+          ),
+        );
+      },
+      separatorBuilder: (BuildContext context, int index) => const Divider(),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Container(
       height: MediaQuery.of(context).size.height,
       width: MediaQuery.of(context).size.width,
-      child: Column(
-        children: const [
-          Text("Automata", style: _biggerFont),
-          Text("Senior Design"),
-          Text("Data Security"),
-        ],
-      )
+      child: _buildClassList(),
     );
   }
 }
