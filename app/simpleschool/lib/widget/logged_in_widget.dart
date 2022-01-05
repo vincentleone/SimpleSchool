5b399f2727015357bc4d34c2fdc60bbde46f9978
gabrielrapaport <gabrielrapaport@gmail.com>
2022-01-05 15:42:04 -0500

    test

--- app/simpleschool/lib/widget/logged_in_widget.dart
+++ app/simpleschool/lib/widget/logged_in_widget.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:simpleschool/widget/weekly_calendar_widget.dart';
@@ -39,6 +41,60 @@ class UserLoggedInWidget extends StatelessWidget {
               height: 8,
             ),
             Text(user.displayName!),
+            SizedBox(
+              height: 50,
+            ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Classes",
+                    style: TextStyle(
+                        fontSize: 16,
+                        fontStyle: FontStyle.normal,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  IconButton(
+                    color: Colors.grey.shade800,
+                    padding: EdgeInsets.all(0),
+                    hoverColor: Colors.blue,
+                    iconSize: 22,
+                    icon: Icon(Icons.add),
+                    onPressed: () {
+                      print("add class");
+                      showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              scrollable: true,
+                              content: Padding(
+                                padding: const EdgeInsets.all(8),
+                                // need to updte to accept class
+                                child: Text("test"),
+                              ),
+                            );
+                          });
+                    },
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              height: 0.5,
+              color: Colors.blueGrey,
+              margin: EdgeInsets.fromLTRB(8, 0, 8, 0),
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Expanded(
+              flex: 80,
+              child: Column(
+                children: [Text("test"), Text("test"), Text("test")],
+              ),
+            ),
           ]),
         ),
       ),
